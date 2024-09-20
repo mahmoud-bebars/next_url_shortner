ced64fa6fda48d92269d0afe459d51cf5d66e3aa
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-09-20 17:37:51 +0300

    (feat): updating RootPage Logic

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ export default async function Home() {
 
   if (!session) {
     redirect("/auth/login");
+  } else {
+    redirect("/dashboard");
   }
+
   return (
-    <main className="mx-auto max-w-xl py-12 md:py-24 space-y-6">
-      <div className="space-y-2 text-cener">
+    <main className="flex items-center justify-center mx-auto max-w-xl py-12 md:py-24 space-y-6">
+      <div className="space-y-2 text-center">
         <h1 className="text-3xl md:text-4xl font-bold ">URL Shortner</h1>
         <p className="md:text-lg">Shorten your URLs & share Them Easily</p>
       </div>
