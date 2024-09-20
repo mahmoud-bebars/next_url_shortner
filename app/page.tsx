016a16dcc87b3da56ed8e7779164ca552c7b870a
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-09-20 17:28:50 +0300

    (feat): adding logic handler to redirect to login when no user founded

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
-import URLShortnerContainer from "@/components/url-shornter-conatiner";
+import { authOptions } from "@/lib/session";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/auth/login");
+  }
   return (
     <main className="mx-auto max-w-xl py-12 md:py-24 space-y-6">
       <div className="space-y-2 text-cener">
         <h1 className="text-3xl md:text-4xl font-bold ">URL Shortner</h1>
         <p className="md:text-lg">Shorten your URLs & share Them Easily</p>
       </div>
-      <URLShortnerContainer />
     </main>
   );
 }
